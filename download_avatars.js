497e1f4372c59bfd195c9a305a8e4d05ff906208
TheTash <t.kashi.92@gmail.com>
2018-01-22 23:30:52 +0000

    mimefied (unparsed view) of the request from download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,18 @@
 var request = require('request');
-
+var authRequest = require('./secrets.js');
 
 console.log("Welcome to GitHub Downloader!");
 
 
 function getRepoContributors(repOwner,repoName, callback){
-  var url = "https://api.github.com/repos/" + repOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+var options = {
+  url : "https://api.github.com/repos/" + repOwner + "/" + repoName + "/contributors",
+  headers : {
+    "User-Agent": 'request',
+    "Authorization": 'token ' + authRequest.GITHUB_TOKEN
+  }
+};
+request(options, function(err, res, body) {
     callback(err, body);
   });
 }

c20d3a3352e18921b04d66ff8eeceed1ad8b8046
TheTash <t.kashi.92@gmail.com>
2018-01-22 21:57:07 +0000

    add the getRepoContributors function

---
+++ download_avatars.js
@@ -0,0 +1,10 @@
+var request = require('request');
+
+
+console.log("Welcome to GitHub Downloader!");
+
+
+function getRepoContributors(repOwner,repoName, callback){
+  console.log("Errors", err);
+  console.log('Results', result);
+}

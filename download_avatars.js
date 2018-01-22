bfa85294f72edde166db0a0af8721c2324b38012
TheTash <t.kashi.92@gmail.com>
2018-01-22 22:09:43 +0000

    gettitng desired error message

--- download_avatars.js
+++ download_avatars.js
@@ -5,6 +5,14 @@ console.log("Welcome to GitHub Downloader!");
 
 
 function getRepoContributors(repOwner,repoName, callback){
+  var url = "https://api.github.com/repos/" + repOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    callback(err, body);
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function (err, result) {
   console.log("Errors", err);
   console.log('Results', result);
-}
+});

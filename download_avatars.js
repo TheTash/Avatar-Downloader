7852c0b2603d850d859ffa81aaa1e68633f8d29d
TheTash <t.kashi.92@gmail.com>
2018-01-23 00:23:57 +0000

    JSON.parse for the body argument in callback

--- download_avatars.js
+++ download_avatars.js
@@ -5,20 +5,30 @@ console.log("Welcome to GitHub Downloader!");
 
 
 function getRepoContributors(repOwner,repoName, callback){
-var options = {
-  url : "https://api.github.com/repos/" + repOwner + "/" + repoName + "/contributors",
-  headers : {
-    "User-Agent": 'request',
-    "Authorization": 'token ' + authRequest.GITHUB_TOKEN
-  }
+
+  var options = {
+    
+
+    url : "https://api.github.com/repos/" + repOwner + "/" + repoName + "/contributors",
+
+    headers : {
+      "User-Agent": 'request',
+      "Authorization": 'token ' + authRequest.GITHUB_TOKEN
+    }
+
+  // Options Object Keys: 1. url, 2. headers
+  // headers Object Keys: 1. User-agent 2. Authorization
+
 };
+
+
 request(options, function(err, res, body) {
-    callback(err, body);
+    callback(err, JSON.parse(body));
   });
 }
 
 
-getRepoContributors("jquery", "jquery", function (err, result) {
+getRepoContributors("jquery", "jquery", function (err, body) {
   console.log("Errors", err);
-  console.log('Results', result);
+  console.log('Results', body);
 });
